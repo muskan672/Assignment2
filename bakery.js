fd9b40bb106f34c8dd6a4fe629069fafa90d2d4d
Muskan <muskanpreetdbl2003@gmail.com>
2022-11-25 15:59:38 -0500

    changes

--- bakery.js
+++ bakery.js
@@ -1,18 +1,2 @@
 
-
-
-    function menu(){
-    document.querySelector(".nav").style.width="250px";
-    document.querySelector("#main").style.marginleft="250px";
-}
-
-
-
-    function navclose(){
-       // alert("meme")
   
-document.getElementsByClassName("nav").style.width="0 ";
-  document.getElementById("main").style.marginleft="0";
-
-}
-
